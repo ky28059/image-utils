88d857b3e4ad6a97539ce5329e979e6ba317c05c
Kevin Yu <kevinyu567@gmail.com>
2025-01-14 09:11:27 -0500

    Exclude AAE files, add some documentation

--- lib/files.ts
+++ lib/files.ts
@@ -2,6 +2,15 @@ import { readdir } from 'node:fs/promises';
 import { BASE_DIR } from '../config';
 
 
+/**
+ * Gets the resolved file name for a given image file. Specifically, if an edited version of an image
+ * exists (e.g. the given file is `IMG_1234` and `IMG_E1234` exists in the directory) return the edited version
+ * instead. Otherwise, return the given file.
+ *
+ * @param file The file to parse (e.g. "IMG_1234.HEIC").
+ * @param dir The list of files in the parsed directory.
+ * @returns The resolved file name.
+ */
 function getResolvedFileName(file: string, dir: string[]) {
     // IMG_1234 -> IMG_E1234
     // p1070837 -> P107E0837
@@ -13,7 +22,7 @@ function getResolvedFileName(file: string, dir: string[]) {
 
 async function getPhotosForDir(dir: string) {
     const files = (await readdir(dir, { withFileTypes: true }))
-        .filter((f) => !f.isDirectory())
+        .filter((f) => !f.isDirectory() && !f.name.endsWith('.AAE'))
         .map((f) => f.name);
 
     return [...new Set(files.map((f) => getResolvedFileName(f, files)))];
