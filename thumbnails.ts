e17ea17e3ee2dff7f4943c1a1ea65b6aedecffcc
Kevin Yu <kevinyu567@gmail.com>
2025-05-30 21:06:08 -0700

    Add SF thumbnail

--- thumbnails.ts
+++ thumbnails.ts
@@ -36,4 +36,5 @@ export const thumbnails: Record<string, string> = {
     '2025-01-04@07 Shanghai': 'IMG_E4106.HEIC',
     '2025-01-07@10 Tokyo': 'IMG_E4174.HEIC',
     '2025-05-20@23 Carlsbad, UCSD, La Jolla': 'IMG_E5688.HEIC',
+    '2025-05-26 San Francisco Chinatown': 'IMG_E5941.HEIC',
 }
