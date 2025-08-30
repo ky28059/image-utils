25b740b00159d2f74c403924c81bc773950be2c0
Kevin Yu <kevinyu567@gmail.com>
2025-08-30 19:18:24 -0400

    Fix URL for album list items

--- app/PhotoAlbumListItem.tsx
+++ app/PhotoAlbumListItem.tsx
@@ -10,7 +10,7 @@ export default function PhotoAlbumListItem(props: PhotoListItemProps) {
     return (
         <Link
             className="px-5 py-3 min-w-80 border border-white/30 hover:border-white/75 transition duration-150 rounded text-left"
-            href={`/${props.name}`}
+            href={`/d/${props.name}`}
         >
             <h3 className="font-medium text-sm">{props.name}</h3>
             <p className="text-sm text-secondary">
