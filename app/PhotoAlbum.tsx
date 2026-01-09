73914bd20f60d799f23556e548df76c8d16af6b3
Kevin Yu <kevinyu567@gmail.com>
2026-01-08 22:14:17 -0800

    Add thumbnail, fix layout inconsistency

--- app/PhotoAlbum.tsx
+++ app/PhotoAlbum.tsx
@@ -11,12 +11,12 @@ type PhotoAlbumProps = {
 export default function PhotoAlbum(props: PhotoAlbumProps) {
     return (
         <Link
-            className="flex flex-col relative w-80 border border-white/30 hover:border-white/75 transition duration-150 rounded overflow-clip text-left"
+            className="flex flex-col relative min-h-48 w-80 border border-white/30 hover:border-white/75 transition duration-150 rounded overflow-clip text-left"
             href={`/d/${props.name}`}
         >
             <img
                 src={fileToS3Url(props.name, props.thumbnail)}
-                className="h-48 w-full object-cover object-[50%_60%]"
+                className="absolute inset-0 h-full w-full object-cover object-[50%_60%]"
                 alt={props.name}
             />
             <div className="absolute bottom-0 inset-x-0 bg-black/65 px-4 py-2">
