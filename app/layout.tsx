524b6e7c67fc722f679720c9017d3b92153c8b93
Kevin Yu <kevinyu567@gmail.com>
2026-03-18 23:02:12 -0400

    Fix layout shift on modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="en" className="dark bg-midnight text-white overflow-y-scroll">
-            <body className={inter.className}>
+        <html lang="en" className="dark bg-midnight text-white">
+            <body className={inter.className + ' overflow-y-scroll'}>
                 <Tooltip.Provider>
                     {children}
                 </Tooltip.Provider>
